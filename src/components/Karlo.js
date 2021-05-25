8598786ff09f79e7bc4ba2ebe6d974f7ca91fe67
Karlo <51051819+Karlo121@users.noreply.github.com>
2021-05-25 18:27:48 +0200

    Karlo done

--- src/components/Karlo.js
+++ src/components/Karlo.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { StyledHeader, ImageWrapper, StyledBox } from "../styles/Global.style";
 import redPowerRanger from "../images/red.jpg";
+import {
+  KarloArticleHeader,
+  KarloArticleText,
+  KarloArticleWrap,
+  KarloImage,
+  KarloImageTwo,
+  OuterWrapper,
+  KontaktWrapper,
+} from "../styles/Karlo.style";
 
 export const Karlo = ({}) => {
   return (
@@ -12,8 +21,53 @@ export const Karlo = ({}) => {
       <StyledHeader>Karlo Balon</StyledHeader>
 
       <StyledBox>
-        <div>hello</div>
-        <div>world</div>
+        <OuterWrapper>
+          <KarloArticleWrap>
+            <KarloArticleHeader>Karlo Balon</KarloArticleHeader>
+            <KarloArticleText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse at velit sit amet neque aliquam viverra. Fusce
+              condimentum nibh ac vulputate semper. Sed eget lacus ultrices,
+              mattis nulla eget, iaculis mi. Phasellus suscipit lectus ipsum,
+              sed vestibulum nisi cursus quis. Mauris finibus maximus massa nec
+              consequat. Sed nec tristique felis. Proin tincidunt turpis
+              molestie egestas ultricies. Pellentesque vel purus lacus.
+              Vestibulum placerat justo leo, vel molestie ex bibendum in.
+            </KarloArticleText>
+            <KarloArticleText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse at velit sit amet neque aliquam viverra. Fusce
+              condimentum nibh ac vulputate semper. Sed eget lacus ultrices,
+              mattis nulla eget, iaculis mi. Phasellus suscipit lectus ipsum,
+              sed vestibulum nisi cursus quis. Mauris finibus maximus massa nec
+              consequat.
+            </KarloArticleText>
+          </KarloArticleWrap>
+          <KarloImage></KarloImage>
+        </OuterWrapper>
+        <OuterWrapper>
+          <KarloImageTwo></KarloImageTwo>
+          <KarloArticleWrap>
+            <KarloArticleHeader>CV</KarloArticleHeader>
+            <KarloArticleText>CV:</KarloArticleText>
+            <KarloArticleText>Portofolio:</KarloArticleText>
+            <KarloArticleHeader>Naslov:</KarloArticleHeader>
+            <KarloArticleText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse at velit sit amet neque aliquam viverra. Fusce
+              condimentum nibh ac vulputate semper. Sed eget lacus ultrices,
+              mattis nulla eget, iaculis mi. Phasellus suscipit lectus ipsum,
+              sed vestibulum nisi cursus quis. Mauris finibus maximus massa nec
+              consequat.
+            </KarloArticleText>
+          </KarloArticleWrap>
+        </OuterWrapper>
+        <KontaktWrapper>
+          <h2>Kontakt</h2>
+          <p>LinkedIn: linkedin/karlo.com</p>
+          <p>E-mail: kbalon@tvz.hr</p>
+          <p>https://github.com/Karlo121</p>
+        </KontaktWrapper>
       </StyledBox>
     </div>
   );
