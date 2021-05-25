4878ec1a5ee749f435f369c98d61f34768949237
Karlo <51051819+Karlo121@users.noreply.github.com>
2021-05-25 19:16:30 +0200

    Lea done

---
+++ src/styles/Lea.style.js
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+import leaImg from "../images/backgrounds/leaslika1.jpg";
+import leaImg2 from "../images/backgrounds/leaslika2.jpg";
+
+export const StyledHeader = styled.h1`
+  text-align: center;
+  font-family: "Courier New", Courier, monospace;
+  color: white;
+`;
+
+export const ImageWrapper = styled.div`
+  margin: 0 auto;
+  text-align: center;
+`;
+
+export const LeaArticleWrap = styled.div`
+  display: flex;
+  height: 400px;
+  width: 600px;
+  border-radius: 15px;
+  margin-bottom: 50px;
+  flex-direction: column;
+  background-color: #737373;
+  box-shadow: 3px 3px 5px 6px #ccc;
+  overflow: auto;
+`;
+
+export const LeaArticleHeader = styled.h1`
+  text-align: center;
+  margin: 0 auto;
+  color: white;
+  margin-top: 15px;
+`;
+
+export const LeaArticleText = styled.h1`
+  font-size: 15px;
+  padding: 10px;
+  color: white;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+`;
+
+export const LeaImage = styled.div`
+  display: flex;
+  height: 400px;
+  width: 500px;
+  border-radius: 15px;
+  margin-left: 50px;
+  justify-content: space-evenly;
+  margin-bottom: 10px;
+  background-image: url(${leaImg});
+  background-size: cover;
+  box-shadow: 3px 3px 5px 6px #ccc;
+`;
+
+export const LeaImageTwo = styled.div`
+  display: flex;
+  height: 400px;
+  width: 500px;
+  border-radius: 15px;
+  justify-content: space-evenly;
+  margin-bottom: 10px;
+  margin-right: 50px;
+  background-image: url(${leaImg2});
+  background-size: cover;
+  box-shadow: 3px 3px 5px 6px #ccc;
+`;
+
+export const OuterWrapper = styled.div`
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+`;
+
+export const KontaktWrapper = styled.div`
+  text-align: center;
+  width: 600px;
+  margin: 0 auto;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+`;
