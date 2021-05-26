1d660dd1350c9e95c08cc00a7a5dc23d79951421
Karlo <51051819+Karlo121@users.noreply.github.com>
2021-05-26 17:05:29 +0200

    Form update

--- src/styles/Kontakt.style.js
+++ src/styles/Kontakt.style.js
@@ -6,4 +6,65 @@ export const KontaktWrapper = styled.div`
   margin-top: 20px;
   margin-bottom: 20px;
   font-family: "Montserrat", sans-serif;
+  border: 1px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+export const Form = styled.form`
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  width: 300px;
+  height: 35px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+`;
+
+export const InputWrapper = styled.div`
+  border: 1px solid red;
+`;
+
+export const Button = styled.button`
+  width: 300px;
+  height: 35px;
+  background-color: #5995ef;
+  color: #fff;
+  border-radius: 3px;
+`;
+
+export const Title = styled.h1`
+  font-family: "Raleway", sans-serif;
+  font-weight: 600;
+  color: #4d4d4d;
+  font-size: 2.2em;
+`;
+
+export const Title2 = styled.h2`
+  font-family: "Raleway", sans-serif;
+  font-weight: 300;
+  color: #4d4d4d;
+  font-size: 1.8em;
+`;
+
+export const Text = styled.p`
+  font-family: "Raleway", sans-serif;
+  color: ${(props) => props.color || "#4d4d4d"};
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  color: #777;
+  font-family: "Raleway", sans-serif;
+  font-size: 0.8em;
+  margin: 0.5em 0;
+  position: relative;
 `;
